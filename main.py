05eaf822e17df3843299bdd67600624f4e08ffad
Andrei Esteban <estebanandrei535@gmail.com>
2026-02-04 09:14:18 +0800

    main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 main.py
-print("Lab1: Version Control")
+print("Lab1: Version Control") 
 print("Author: "ANDREI ZACHARY V. ESTEBAN")
 print("Student ID: TUPM-25-4781")
 print("Class Section: BSECE - 1A")
